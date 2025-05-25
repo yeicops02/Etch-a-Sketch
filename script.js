e2f7dec0bdce6a4604dd9ea51316d9d3d38624b7
yeicops02 <158612301+yeicops02@users.noreply.github.com>
2025-05-24 18:42:16 -0600

    UI y lógica básica ya esta construido

---
+++ script.js
@@ -0,0 +1,21 @@
+const contenedor = document.querySelector('.container')
+
+
+for (let i = 0; i < 16; i++ ) {
+    const row = document.createElement('div');  //H
+    row.classList.add('row');
+    
+    for (let j = 0; j<16; j++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+    contenedor.appendChild(row);
+}
+
+function SolicitarNumero () {
+    let alerta = parseInt(prompt('Introduce de que tamaño quieres la cuadricula. Proporciona un solo número'))
+    return alerta
+}
+
+
