5be9ad95c0d012c1c88fee364da983e9b030033c
yeicops02 <158612301+yeicops02@users.noreply.github.com>
2025-05-24 18:43:01 -0600

    modificacion archivo js

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ for (let i = 0; i < 16; i++ ) {
 }
 
 function SolicitarNumero () {
-    let alerta = parseInt(prompt('Introduce de que tamaño quieres la cuadricula. Proporciona un solo número'))
+    let alerta = parseInt(prompt('Introduce de que tamaño quieres la cuadricula. Proporciona un solo número del 1-100'))
     return alerta
 }
 
+
+function GeneradorCuadriculas () {
+    const numeroCuadricula = SolicitarNumero()
+for (let i = 0; i < numeroCuadricula; i++ ) {
+    const row = document.createElement('div');  //H
+    row.classList.add('row');
+    
+    for (let j = 0; j < numeroCuadricula; j++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+    contenedor.appendChild(row);
+}
+}
 
